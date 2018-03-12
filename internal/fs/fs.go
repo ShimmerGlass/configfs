5314eda1be5488539f76b0ea09ea6fc782fda85c
Thibault Gilles <tgilles@molotov.tv>
2018-03-12 13:11:16 +0100

    Fuse fixes

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -26,9 +26,12 @@ func Run(mountPoint string, cb func(path string) ([]byte, error)) error {
 	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-signalChannel
-		log.Println("Unmounting")
-		log.Fatal("end", server.Unmount())
+		log.Println("unmounting")
+		if err := server.Unmount(); err != nil {
+			log.Println(err)
+		}
 	}()
+	log.Println("mounting on", mountPoint)
 	server.Serve()
 	time.Sleep(time.Second)
 	return nil
@@ -40,17 +43,39 @@ type fs struct {
 }
 
 func (me *fs) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
-	return &fuse.Attr{
-		Mode: fuse.S_IFREG | 0644, Size: uint64(len(name)),
-	}, fuse.OK
+	switch name {
+	case "":
+		return &fuse.Attr{
+			Mode: fuse.S_IFDIR | 0755,
+		}, fuse.OK
+	default:
+		path, err := base64.URLEncoding.DecodeString(name)
+		if err != nil {
+			log.Println(err)
+			return nil, fuse.ENOENT
+		}
+
+		contents, err := me.cb(string(path))
+		if err != nil {
+			log.Println(err)
+			return nil, fuse.ENOENT
+		}
+
+		return &fuse.Attr{
+			Mode: fuse.S_IFREG | 0644, Size: uint64(len(contents)),
+		}, fuse.OK
+	}
 }
 
 func (me *fs) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry, code fuse.Status) {
-	return nil, fuse.ENOENT
+	return []fuse.DirEntry{}, fuse.OK
+}
+
+func (fs *fs) Access(name string, mode uint32, context *fuse.Context) (code fuse.Status) {
+	return fuse.OK
 }
 
 func (me *fs) Open(name string, flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
-	log.Println("fuse file access", name)
 	path, err := base64.URLEncoding.DecodeString(name)
 	if err != nil {
 		log.Println(err)
